8fc3219b5bcb632f698dbdc4084e9ada69117384
Carla Herrán <carlaherran@MacBook-Pro-de-Carla.local>
2017-08-04 14:45:33 -0500

    Abuelita asinc empezando

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
+console.log('Document');
+
 $(document).ready(function() {
 	// Este código corre después de que `document` fue cargado(loaded) 
 	// completamente. 
 	// Esto garantiza que si amarramos(bind) una función a un elemento 
 	// de HTML este exista ya en la página. 
-
+	console.log('Document ready');
+	$("#formulario").submit(function(event){
+		event.preventDefault();
+		console.log('Default prevented');
+		var form_data = $('#formulario').serialize();
+		console.log('form_data',form_data);
+	});
 });
