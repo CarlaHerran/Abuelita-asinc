f2bbbe10eed5749e29b38adf3f2e70ce255d14c0
Carla Herrán <carlaherran@MacBook-Pro-de-Carla.local>
2017-08-04 16:58:22 -0500

    Deaf grandma asinc terminado y comentado

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,16 +11,16 @@ post '/abuelita' do
 
   # Iniciamos la condición para cuando le hablemos quedito (minúsculas), ya que no nos va a escuchar bien.
     if user_input == user_input.downcase
-      abuelita = "HUH?! NO TE ESCUCHO, HIJO!"
-    # Continuamos nuestra condición para cuando le hablemos fuerte (mayúsculas), y mientras lo que le digamos sea diferente a la despedida, la abuelita responderá:  
+      respuesta = "HUH?! NO TE ESCUCHO, HIJO!"
+    # Continuamos nuestra condición para cuando le hablemos fuerte (mayúsculas), y mientras lo que le digamos sea diferente a la despedida, la respuesta responderá:  
     elsif user_input == user_input.upcase && user_input != "BYE TQM"
-      abuelita = "NO, NO DESDE 1983"
-    # De no ser así; cuando nos despidamos, la abuelita preguntará "Que dijiste?", hasta que se lo digamos tres veces.
+      respuesta = "NO, NO DESDE 1983"
+    # De no ser así; cuando nos despidamos, la respuesta preguntará "Que dijiste?", hasta que se lo digamos tres veces.
     else 
-      abuelita = "Adiós hijo" 
+      respuesta = "Adiós hijo" 
     end
-
-  redirect "/?abuelita=#{abuelita}"
+    respuesta
+  #redirect "/?abuelita=#{respuesta}"
 
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -5,11 +5,28 @@ $(document).ready(function() {
 	// completamente. 
 	// Esto garantiza que si amarramos(bind) una función a un elemento 
 	// de HTML este exista ya en la página. 
-	console.log('Document ready');
+
+	// Seleccionar el formulario y puse un listener para cuando le den submit al formulario.
 	$("#formulario").submit(function(event){
+		// Hay que detener el funcionamiento por default del formulario.
 		event.preventDefault();
-		console.log('Default prevented');
+		//console.log('Default prevented');
+
+		// Se le da formato a la informacion del formulario con serialize.
 		var form_data = $('#formulario').serialize();
-		console.log('form_data',form_data);
+		//console.log('form_data',form_data);
+
+		// Hicimos un pedido de ajax tipo POST con los datos del formulario.
+		$.post( "/abuelita", form_data, function(data){
+			//console.log(data);
+
+			// Mostramos el div de la respuesta.
+			$('#divrespuesta').show();
+			// Después ponemos el resultado (data) en el span grandma_says.
+			$('#grandma_says').html(data);
+		});
+
 	});
+
+
 });
